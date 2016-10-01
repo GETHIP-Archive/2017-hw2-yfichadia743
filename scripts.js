d998774e6122c929e3ac4bcc77daa610a6b106f8
yfichadia743 <yash.fichadia@gmail.com>
2016-10-01 12:11:52 -0500

    Message

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $.getJSON("C:\Users\ashokf\Desktop\2017-hw2-yfichadia743\yash.json", function(data){
+  $.getJSON("https://raw.githubusercontent.com/GETHIP-Classroom/2017-hw2-yfichadia743/master/yash.json", function(data){
     console.log(data);
     $("#profile-name").text(data.firstName + " " + data.lastName);
     $("#age").text(data.age);
